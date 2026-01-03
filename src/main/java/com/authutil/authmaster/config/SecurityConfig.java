af5627baf9672c3c12e7775a1429283b3662a709
Anurag Patil <patilanurag661@gmail.com>
2026-01-03 11:57:31 +0530

    Added cors for frontend

--- src/main/java/com/authutil/authmaster/config/SecurityConfig.java
+++ src/main/java/com/authutil/authmaster/config/SecurityConfig.java
@@ -49,7 +49,8 @@ public class SecurityConfig {
         config.setAllowedOrigins(List.of(
                 "http://localhost:3000",
                 "http://localhost:5173",
-                "http://192.168.1.3:5173"
+                "http://192.168.1.3:5173",
+                "https://devendpoint.guru"
         ));
         config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         config.setAllowedHeaders(List.of("*"));
